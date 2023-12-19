c7727284e8f98fa9fed7c059eb1c58af1342d827
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2023-12-19 13:56:24 +0000

    Chtoto

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,9 +1,12 @@
 package rvt;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
@@ -13,16 +16,33 @@ public class DefaultController {
         return "index";
     }
 
+    // @GetMapping(value = "/about")
+    // String aboutme(Model model){
+    //     String name="Patricija";
+    //     String surname="Griņičeva";
+    //     int age = 16;
+    //     String group="DP2-1";
+    //     model.addAttribute("name",name);
+    //     model.addAttribute("surname",surname);
+    //     model.addAttribute("age",age);
+    //     model.addAttribute("group",group);
+    //     return "about";
+    // }
+
     @GetMapping(value = "/about")
-    String aboutme(Model model){
-        String name="Patricija";
-        String surname="Griņičeva";
-        int age = 16;
-        String group="DP2-1";
-        model.addAttribute("name",name);
-        model.addAttribute("surname",surname);
-        model.addAttribute("age",age);
-        model.addAttribute("group",group);
-        return "about";
+    ModelAndView about(
+        
+            @RequestParam HashMap<String, String> allParams
+    
+        ) {
+            // String name=allParams.get("name");
+            // String age=allParams.get("age");
+            ModelAndView modelAndView = new ModelAndView("about");
+            // modelAndView.addObject("name", name);
+            // modelAndView.addObject("age", age);
+
+            return modelAndView;
     }
+
+
 }
