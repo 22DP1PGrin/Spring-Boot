b3ce7e93c4d17f07bdb304d7f10fc40c4b1b8976
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-01-31 12:52:36 +0000

    Sort 4

--- src/main/java/rvt/Sorter.java
+++ src/main/java/rvt/Sorter.java
@@ -12,6 +12,30 @@ public abstract class Sorter {
        return small;
     }  
     public static int indexOfSmallest(int[] array){
-        
+        int smallIndex=0;
+        for(int i=1; i<array.length;i++){
+            if(array[i]<array[smallIndex]){
+                smallIndex=i;
+            }
+        } 
+        return smallIndex;  
     }
+    public static int indexOfSmallestFrom(int[] table, int startIndex){
+       int minSk=table[startIndex];
+       int minIndex=startIndex;
+       for(int i=startIndex; i<table.length;i++){
+            if(table[i]<minSk){
+                minIndex=i;
+            }
+       } 
+       return minIndex;
+    }
+    public static void swap(int[] array, int index1, int index2) {
+        int arr=array[index1];
+        array[index1]=array[index2];
+        array[index2]=arr;    
+    } 
+    public static void sort(int[] array) {
+
+    }  
 }
--- src/test/java/lv/rvt/SorterTets.java
+++ src/test/java/lv/rvt/SorterTets.java
@@ -1,12 +1,40 @@
 package lv.rvt;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import rvt.Sorter;
-public abstract class SorterTets {
+
+
+public class SorterTets {
     @Test
     public void testSmallest(){
         int[] intArray={3,4,-4,6,56,4,12};
         assertEquals(-4,Sorter.smallest(intArray));
     }
+    @Test
+    public void testIndeOfSmallest(){
+        int[] intArray={-5,4,-4,6,56,4,12};
+        assertEquals(0,Sorter.indexOfSmallest(intArray));
+    }
+    @Test
+    public void testIndexOfSmallestFrom(){
+        int[] intArray={3,4,-4,7,16,54,12};
+        assertEquals(2,Sorter.indexOfSmallestFrom(intArray, 1));
+        assertEquals(6,Sorter.indexOfSmallestFrom(intArray, 4));
+        assertEquals(3,Sorter.indexOfSmallestFrom(intArray, 3));
+        assertEquals(2,Sorter.indexOfSmallestFrom(intArray, 2));
+        
+    }
+    @Test
+    public void testSwap(){
+        int[] intArray={3,4,-4,7,};
+        int[] intSwapArray={3,-4,4,7};
+        Sorter.swap(intArray, 1, 2);
+        assertArrayEquals(intSwapArray, intArray);
+        
+        
+    }
 }
+
